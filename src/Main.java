359668aff7fbb46c9137c4fbd8acbdd0bf9156a0
Jose Benitez <jbenitez86@live.com>
2022-09-06 07:14:14 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//Import the Scanner utility
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Create a scanner
+        Scanner input = new Scanner(System.in);
+
+        //Create a 2-Dimensional Array to read in the integer values for the program to read and check
+        int[][] table =
+    }
+
+    public static boolean isConsecutiveFour(int[][] values){
+
+    }
+}
